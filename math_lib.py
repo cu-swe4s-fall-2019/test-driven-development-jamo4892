8703ae11872df7db6ddaca9294104c1e1ea97d78
Jamie Mothersbaugh <james.mothersbaugh@colorado.edu>
2019-09-27 04:18:11 +0000

    Made changes to documentation

--- math_lib.py
+++ math_lib.py
@@ -1,5 +1,20 @@
-"""This script takes a numerical array as input and returns either the mean
-or the standard deviation of the array."""
+"""
+Purpose
+-------
+This script takes a numerical array as input and returns either the mean
+or the standard deviation of the array.
+
+Inputs
+------
+L: Integer list
+List of integer values
+
+Outputs
+-------
+Mean: mean of the input list
+
+Stdev: standard deviation of the input list
+"""
 
 import math
 # Import modules
